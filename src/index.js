0272196bd395c147b315e56d04ad46d691f18b3d
Konrad <konrad.babowicz21@gmail.com>
2023-12-14 22:02:52 +0000

    added xml and csv files that show in console when using JS file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import myName from './myName';
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.jpeg';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
     const element = document.createElement('div');
@@ -13,6 +15,10 @@ function component() {
     myIcon.src = Icon;
 
     element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
+
 
     return element;
   }
