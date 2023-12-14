8dc841dd4928f0a6656907e617d51e491ecb4b79
Konrad <konrad.babowicz21@gmail.com>
2023-12-14 21:36:52 +0000

    css style works now with js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import myName from './myName';
 import _ from 'lodash';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
   
     element.textContent = myName("Cody") ; 
+    element.classList.add('hello');
     return element;
   }
   
