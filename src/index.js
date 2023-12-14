3e6b06ac868f197875412439f2c0b7e418b29295
Konrad <konrad.babowicz21@gmail.com>
2023-12-14 21:43:15 +0000

    added a picture

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import myName from './myName';
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.jpeg';
 
 function component() {
     const element = document.createElement('div');
   
     element.textContent = myName("Cody") ; 
     element.classList.add('hello');
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
     return element;
   }
   
